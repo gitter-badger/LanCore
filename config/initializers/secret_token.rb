ceba827210371d0ab8f98693a3735e2ba0850f09
Misha Prasolov <mishanet123@gmail.com>
2015-07-01 01:52:59 -0500

    Initial generated docs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,6 @@
+# config/initializers/secret_token.rb
+Rails.application.configure do
+  # config.secret_token = 'existing secret token'
+  config.secret_key_base = '3f25c7efd40723bdf9745182e40e93318b3a37f5ce820f6f048e42809092144ebe5d1331889e453a30ecd7f4ccf7a7ece23da2537ffa9d34f36f6321903
+e2909'
+end
