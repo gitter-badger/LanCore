ec560257980e3198a87e37ef57dd773aaebb9aa0
Misha Prasolov <mishanet123@gmail.com>
2015-07-01 01:50:14 -0500

    App-wide refactor

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module LanCore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # config.assets.precompile += %w( *.js *.css )
+
+    # config.serve_static_files = true
   end
 end
