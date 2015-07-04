a3d7c765fe088399ffd5d3eb780e7a2fbe505d82
Misha Prasolov <mishanet123@gmail.com>
2015-07-04 01:39:38 -0500

    Remodeling homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,12 @@ module ApplicationHelper
     if pagename === 'Home'
       return 'LanCore'
     end
-  else
     return pagename+' - LanCore'
+  end
+  def footerhelper(pagename)
+    if pagename === 'Home'
+      return 'margin-top: 0;'
+    end
+    return ''
   end
 end
