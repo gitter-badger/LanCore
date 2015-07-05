1e4e6ec0632b68365f9815853d8c46bd613ed531
amd1250x <aaron.m.dsa@gmail.com>
2015-07-05 16:38:06 -0500

    Added routes for features and about.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @pagename = 'Home';
   end
+  def about
+    @pagename = 'About';
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :parties
   get 'home' => 'home#index'
+  get 'about' => 'home#about'
+  get 'features' => 'home#features'
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
